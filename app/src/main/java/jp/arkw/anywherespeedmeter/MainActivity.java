77734bb0449990aa1663d9dd6394070671d67c95
Sora Arakawa <mail@arkw.net>
2025-04-19 15:45:57 +0900

    Display speed/latitude/longitude

--- app/src/main/java/jp/arkw/anywherespeedmeter/MainActivity.java
+++ app/src/main/java/jp/arkw/anywherespeedmeter/MainActivity.java
@@ -1,14 +1,49 @@
 package jp.arkw.anywherespeedmeter;
 
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.Manifest;
+import android.os.Looper;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+
 public class MainActivity extends AppCompatActivity {
+    private boolean permission = false;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private TextView textViewSpeed;
+    private TextView textViewLatitude;
+    private TextView textViewLongitude;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+        new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                permission = true;
+            } else {
+                showAlert(getString(R.string.alert_permission));
+            }
+        }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +55,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textViewSpeed = findViewById(R.id.text_view_speed);
+        textViewLatitude = findViewById(R.id.text_view_latitude);
+        textViewLongitude = findViewById(R.id.text_view_longitude);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            permission = true;
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (permission == true) {
+            locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+                .setMinUpdateIntervalMillis(1000)
+                .build();
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(@NonNull LocationResult locationResult) {
+                    for (Location location : locationResult.getLocations()) {
+                        textViewSpeed.setText("" + (int)Math.floor(location.getSpeed()));
+                        textViewLatitude.setText(String.format("%.07f", location.getLatitude()));
+                        textViewLongitude.setText(String.format("%.07f", location.getLongitude()));
+                    }
+                }
+            };
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+        }
+    }
+
+    private void showAlert(String text) {
+        new AlertDialog.Builder(this)
+            .setTitle("")
+            .setMessage(text)
+            .setPositiveButton("OK", null)
+            .show();
     }
 }
